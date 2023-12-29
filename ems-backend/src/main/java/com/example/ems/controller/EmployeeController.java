c49902754014ca8220995bebfde9adce0d77760d
Berlin Bruno <132148418+berlinbruno@users.noreply.github.com>
2023-12-29 21:43:16 +0530

    Update EmployeeController.java

--- ems-backend/src/main/java/com/example/ems/controller/EmployeeController.java
+++ ems-backend/src/main/java/com/example/ems/controller/EmployeeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
-@CrossOrigin(origins = "http://localhost:5173/")
+@CrossOrigin(origins = "https://ems123.netlify.app/")
 @RestController
 @RequestMapping("/api/employees")
 public class EmployeeController {
